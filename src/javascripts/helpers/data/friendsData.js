6a7e7a520bf77a6e691ea5c94809b4a0bff99946
Lwam Tekie <ltekie87@yahoo.com>
2019-06-01 09:47:57 -0500

    added friends

--- src/javascripts/helpers/data/friendsData.js
+++ src/javascripts/helpers/data/friendsData.js
@@ -3,7 +3,21 @@ import axios from 'axios';
 import apiKeys from '../apiKeys.json';
 
 const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
-
 const addNewFriend = friendObject => axios.post(`${firebaseUrl}/friends.json`, friendObject);
 
-export default { addNewFriend };
+
+const getFriendsByUid = uid => new Promise((resolve, reject) => {
+  axios.get(`${firebaseUrl}/friends.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((results) => {
+      const friendsResults = results.data;
+      const friends = [];
+      Object.keys(friendsResults).forEach((friendsId) => {
+        friendsResults[friendsId].id = friendsId;
+        friends.push(friendsResults[friendsId]);
+      });
+      resolve(friends);
+    })
+    .catch(err => reject(err));
+});
+
+export default { addNewFriend, getFriendsByUid };
